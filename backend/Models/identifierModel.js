bf2e47991b027e98e0c8bd5bc7cdfc248c411868
chingchieh <tsao.seaurchin@gmail.com>
2023-06-01 15:37:22 +0800

    change the column name of Identifier Table

--- backend/Models/identifierModel.js
+++ backend/Models/identifierModel.js
@@ -1,14 +1,14 @@
 //Identifier model
 module.exports = (sequelize, DataTypes) => {
-   const Identifier = sequelize.define( "identifiers", {
-       identifier: {
-           type: DataTypes.STRING,
-           allowNull: false
-       },
-       entity_type: {
-           type: DataTypes.STRING,
-           allowNull: false
-       }
-   }, {timestamps: false}, )
-   return Identifier
-}
+    const Identifier = sequelize.define( "identifiers", {
+        identifier: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        entity_type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {timestamps: false}, )
+    return Identifier
+ }
